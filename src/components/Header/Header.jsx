478b9de74bc3cb1f76a56670480ce50dddb08e7c
polin103109 <sabrinpolinece18@gmail.com>
2023-05-06 01:38:39 +0600

    added css style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-
-
+import Logo from "../svg";
 import './Header.css';
+
 const Header = () =>{
 return (
-   <nav>
-   
+   <nav className="header">
+   <Logo />
     <div>
         <Link to="/shop">Shop</Link>
         <Link to="/orders">Orders</Link>
